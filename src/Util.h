fed172037a1feb2beba7d345f5d97dd330878536
Jae-Yong Choi <jayong93@gmail.com>
2017-12-11 19:46:58 +0900

    VBuffer 구현

--- src/Util.h
+++ src/Util.h
@@ -1,2 +1,13 @@
 void* MovePointer(char** ptr, int offset);
 void* MovePointerStep(char** ptr, int step, int offset);
+
+typedef struct VBuffer_ {
+	char* ptr;
+	size_t maxLen;
+	size_t len;
+} VBuffer;
+
+VBuffer VBCreate(size_t size);
+int VBAppend(VBuffer* buf, char* source, size_t size);
+int VBReplace(VBuffer* buf, char* source, size_t size);
+void VBDestroy(VBuffer* buf);
