46d54a3c18bbf1cda4326c45ae0724e15ca49a71
Jae-Yong Choi <jayong93@gmail.com>
2017-12-11 19:46:58 +0900

    VBuffer 구현

--- src/Util.c
+++ src/Util.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include "Util.h"
 
 void* MovePointer(char** ptr, int offset) {
@@ -9,4 +11,54 @@ void* MovePointer(char** ptr, int offset) {
 
 void* MovePointerStep(char** ptr, int step, int count) {
 	return MovePointer(ptr, step*count);
+}
+
+VBuffer VBCreate(size_t size) {
+	VBuffer vb;
+	if (size == 0) {
+		memset(&vb, 0, sizeof(vb));
+		return vb;
+	}
+
+	vb.ptr = (char*)malloc(size);
+	vb.maxLen = size;
+	vb.len = 0;
+
+	return vb;
+}
+
+int VBAppend(VBuffer* buf, char* source, size_t size) {
+	if (buf->ptr == NULL) return 0;
+	if (source == NULL) return 0;
+	if (size == 0) return 1;
+
+	if ((buf->maxLen - buf->len) < size) {
+		size_t newSize = (size>buf->maxLen*2)?(int)size*1.5:buf->maxLen*2;
+		buf->maxLen = newSize;
+		buf->ptr = (char*)realloc(buf->ptr, newSize);
+	}
+	memcpy(buf->ptr + buf->len, source, size);
+	buf->len += size;
+}
+
+int VBReplace(VBuffer* buf, char* source, size_t size) {
+	if (buf->ptr == NULL) return 0;
+	if (source == NULL) return 0;
+	if (size == 0) return 1;
+	
+	if (buf->maxLen < size) {
+		size_t newSize = (size>buf->maxLen*2)?(int)size*1.5:buf->maxLen*2;
+		buf->maxLen = newSize;
+		free(buf->ptr);
+		buf->ptr = (char*)malloc(newSize);
+	}
+	memcpy(buf->ptr, source, size);
+	buf->len = size;
+}
+
+void VBDestroy(VBuffer* buf) {
+	if (buf->ptr) {
+		free(buf->ptr);
+	}
+	memset(buf, 0, sizeof(*buf));
 }
